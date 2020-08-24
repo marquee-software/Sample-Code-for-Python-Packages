0fd54c84dd755152459204318455327847916c4e
Highering AI <34322145+marquee-software@users.noreply.github.com>
2020-08-24 00:16:48 -0700

    Add files via upload

---
+++ me.py
@@ -0,0 +1,14 @@
+import urllib
+import urllib.request
+from bs4 import BeautifulSoup
+
+
+theurl = "https://twitter.com/RanjSriv"
+thepage = urllib.request.urlopen(theurl)
+soup = BeautifulSoup(thepage,"html.parser")
+
+#print(soup.title.text)
+#print (soup.findAll('a'))
+#for link in soup.findAll('a'):
+#    print (link.get('href'))
+print (soup.find('div',{"class":"ProfileHeaderCard"}).find('p').text)
