9ce7c40893b7905e302a8afd9066570679051127
marquee-software <marquee.software@yahoo.com>
2020-08-28 04:51:25 -0700

    what

--- me.py
+++ me.py
@@ -11,4 +11,4 @@ soup = BeautifulSoup(thepage,"html.parser")
 #print (soup.findAll('a'))
 #for link in soup.findAll('a'):
 #    print (link.get('href'))
-print (soup.find('div',{"class":"ProfileHeaderCard"}).find('p').text)
+print (soup.find('div',{"class":"ProfileHeaderCard"}))
